7b7d92d4ec063cfeb1d17f2df33788a23c746766
Eli <eliana.leguizamon@gmail.com>
2020-11-20 19:49:05 -0300

    first commit

---
+++ ejercicio-mostrar-en-consola/prueba.js
@@ -0,0 +1,13 @@
+console.log('Eli Leguizamon');
+console.log(5 + 5);
+
+const array = ["Los Soprano", "The wire", "Fleabag"];
+
+const objeto = {
+  serie1 : "Los soprano",
+  serie2 : "The wire",
+  serie3 : "Fleabag"
+}
+
+console.log(objeto);
+console.log(Object.values(objeto));
